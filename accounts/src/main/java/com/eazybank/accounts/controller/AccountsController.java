321770477e02e3e39adfc797603d19a81fa332c5
fatihduzenli <fatihdznl1991@gmail.com>
2024-11-01 03:04:53 -0400

    Validations are added

--- accounts/src/main/java/com/eazybank/accounts/controller/AccountsController.java
+++ accounts/src/main/java/com/eazybank/accounts/controller/AccountsController.java
@@ -4,22 +4,24 @@ import com.eazybank.accounts.constants.AccountsConstants;
 import com.eazybank.accounts.dto.CustomerDto;
 import com.eazybank.accounts.dto.ResponseDto;
 import com.eazybank.accounts.service.IAccountService;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(path = "/api", produces = {MediaType.APPLICATION_JSON_VALUE})
-
+@Validated
+@AllArgsConstructor
 public class AccountsController {
 
     private final IAccountService iAccountsService;
 
-    public AccountsController(IAccountService iAccountsService) {
-        this.iAccountsService = iAccountsService;
-    }
+
 
 
     /**
@@ -29,7 +31,7 @@ public class AccountsController {
      * @return A ResponseEntity containing a ResponseDto with status code 201 and message "Account created successfully".
      */
     @PostMapping("/create")
-    public ResponseEntity<ResponseDto> createAccount(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> createAccount(@Valid @RequestBody CustomerDto customerDto) {
         iAccountsService.createAccount(customerDto);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -37,7 +39,8 @@ public class AccountsController {
     }
 
     @GetMapping("/fetch")
-    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam String mobileNumber) {
+    public ResponseEntity<CustomerDto> fetchAccountDetails(@RequestParam
+                            @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number should be 10 digits") String mobileNumber) {
         CustomerDto customerDto = iAccountsService.fetchAccountDetails(mobileNumber);
 
         return ResponseEntity.status(HttpStatus.OK)
@@ -51,7 +54,7 @@ public class AccountsController {
      * @return A ResponseEntity containing a ResponseDto with status code 200 if the account is successfully updated, or 500 if an error occurs during the update process.
      */
     @PutMapping("/update")
-    public ResponseEntity<ResponseDto> updateAccountDetails(@RequestBody CustomerDto customerDto) {
+    public ResponseEntity<ResponseDto> updateAccountDetails(@Valid @RequestBody CustomerDto customerDto) {
         boolean isUpdated = iAccountsService.updateAccount(customerDto);
         if(isUpdated) {
             return ResponseEntity
@@ -65,7 +68,7 @@ public class AccountsController {
     }
 
     @DeleteMapping("/delete")
-    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam String mobileNumber) {
+    public ResponseEntity<ResponseDto> deleteAccount(@RequestParam  @Pattern(regexp = "^[0-9]{10}$", message = "Mobile number should be 10 digits") String mobileNumber) {
         boolean isDeleted = iAccountsService.deleteAccount(mobileNumber);
         if(isDeleted) {
             return ResponseEntity
--- accounts/src/main/java/com/eazybank/accounts/dto/AccountsDto.java
+++ accounts/src/main/java/com/eazybank/accounts/dto/AccountsDto.java
@@ -1,12 +1,19 @@
 package com.eazybank.accounts.dto;
 
 import jakarta.persistence.Column;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class AccountsDto {
-
+    @NotEmpty(message = "Account holder name cannot be empty")
+    @Pattern(regexp = "^[0-9]{10}$", message = "Account holder name should be 10 digits")
     private Long accountNumber;
+
+    @NotEmpty(message = "Account type cannot be empty")
     private String accountType;
+
+    @NotEmpty(message = "Branch address cannot be empty")
     private String branchAddress;
 }
