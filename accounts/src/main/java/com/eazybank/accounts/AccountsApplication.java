44e394aad6b535b6625a87ad0539a92736e91bc3
fatihduzenli <fatihdznl1991@gmail.com>
2024-11-01 03:06:20 -0400

    JPA Auditing enabled

--- accounts/src/main/java/com/eazybank/accounts/AccountsApplication.java
+++ accounts/src/main/java/com/eazybank/accounts/AccountsApplication.java
@@ -2,12 +2,18 @@ package com.eazybank.accounts;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
 
 @SpringBootApplication
+@EnableJpaAuditing(auditorAwareRef = "auditAwareImpl")
 public class AccountsApplication {
+
 
     public static void main(String[] args) {
         SpringApplication.run(AccountsApplication.class, args);
+
     }
 
 }
