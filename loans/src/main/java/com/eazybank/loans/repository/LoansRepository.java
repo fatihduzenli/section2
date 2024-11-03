9d58c02469b5295aa22d06447b8ecb54859f7379
fatihduzenli <fatihdznl1991@gmail.com>
2024-11-03 01:51:07 -0400

    LoansRepository created

---
+++ loans/src/main/java/com/eazybank/loans/repository/LoansRepository.java
@@ -0,0 +1,11 @@
+package com.eazybank.loans.repository;
+
+import com.eazybank.loans.entity.Loans;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface LoansRepository extends JpaRepository<Loans, Long> {
+
+    Optional<Loans> findByMobileNumber(String mobileNumber);
+}
